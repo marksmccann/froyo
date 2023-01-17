241530c321a0ae7614a46e68122cdee852409a79
Mark McCann <mail@markmccann.me>
2023-01-17 00:06:37 -0600

    chore: install jest and testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'plugin:jest-dom/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'jest-dom'],
+    rules: {},
+    overrides: [
+        {
+            files: ['jest-setup.js'],
+            rules: {
+                'import/no-extraneous-dependencies': 'off',
+            },
+        },
+    ],
 };
