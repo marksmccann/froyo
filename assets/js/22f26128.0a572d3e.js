ab8f41fe87dc68687f06af42d042ac0edcb88f0e
marksmccann <marksmccann@users.noreply.github.com>
2023-01-18 01:39:29 +0000

    deploy: a7f2296f4498edd14c7f54719ba24a721cff1646

---
+++ assets/js/22f26128.0a572d3e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfroyo=self.webpackChunkfroyo||[]).push([[6052],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- packages/froyojs/README.md
+++
@@ -1,3 +0,0 @@
-# Froyo
-
-`Froyo` is a framework for building user interfaces with vanilla JavaScript.
